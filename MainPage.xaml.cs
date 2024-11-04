2f72abfe7cd86dee3e38fe12cce9b952a2ce429e
Usuario <Usuario@CPD03-M13>
2024-11-04 16:00:16 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
